f92f7c81e18cef7590b9dcb1e07295482843be9e
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-06-01 02:06:05 +0800

    Create the routes

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+use Phalcon\Mvc\Micro;
+
+$app = new Micro();
+
+// Retrieves all robots
+$app->get(
+    '/api/robots',
+    function() {
+
+    }
+);
+
+// Searches for robots with $name in their name
+$app->get(
+    '/api/robots/search/{name}',
+    function($name) {
+
+    }
+);
+
+// Retrieves robots based on primary key
+$app->get(
+    '/api/robots/{id:[0-9]+}',
+    function($id) {
+
+    }
+);
+
+// Adds a new robot
+$app->post(
+    '/api/robots',
+    function() {
+
+    }
+);
+
+// Updates robots based on primary key
+$app->put(
+    '/api/robots/{id:[0-9]+}',
+    function($id) {
+
+    }
+);
+
+// Deletes robots based on primary key
+$app->delete(
+    '/api/robots/{id:[0-9]+}',
+    function($id) {
+        
+    }
+);
+
+$app->handle();
+
+?>
