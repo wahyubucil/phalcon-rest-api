19f2878b1af1aeb8a6724017d5bdb707e96351c1
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-06-01 16:35:37 +0800

    Implement PUT /api/robots/{id}

--- index.php
+++ index.php
@@ -181,8 +181,46 @@ $app->post(
 // Updates robots based on primary key
 $app->put(
     '/api/robots/{id:[0-9]+}',
-    function($id) {
+    function($id) use ($app) {
+        $robot = $app->request->getJsonRawBody();
 
+        $phql = 'UPDATE Store\Toys\Robots SET name = :name:, type = :type:, year = :year: WHERE id = :id:';
+
+        $status = $app->modelsManager->executeQuery(
+            $phql,
+            [
+                'id' => $id,
+                'name' => $robot->name,
+                'type' => $robot->type,
+                'year' => $robot->year
+            ]
+        );
+
+        // Create a response
+        $response = new Response();
+
+        // Check if the insertion was successful
+        if ($status->success() === true) {
+            $response->setJsonContent([
+                'status' => 'OK'
+            ]);
+        } else {
+            // Change the HTTP status
+            $response->setStatusCode(409, 'Conflict');
+
+            $errors = [];
+
+            foreach ($response->getMessages() as $message) {
+                $errors[] = $message->getMessage();
+            }
+
+            $response->setJsonContent([
+                'status' => 'ERROR',
+                'messages' => $errors
+            ]);
+        }
+
+        return $response;
     }
 );
 
